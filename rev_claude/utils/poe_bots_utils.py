c3a4d65d5cedbf34c4167d994ca37850a2314c87
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2024-10-16 23:59:51 +0800

    Update poe_bots_utils.py

--- rev_claude/utils/poe_bots_utils.py
+++ rev_claude/utils/poe_bots_utils.py
@@ -5,7 +5,10 @@ from rev_claude.configs import POE_BOT_INFO
 
 @lru_cache
 def get_poe_bot_info():
-    return json.loads(POE_BOT_INFO.read_text())
+    res =  json.loads(POE_BOT_INFO.read_text())
+    # change each the key in the dict to lower case
+    res = {k.lower(): v for k, v in res.items()}
+    return res
 
 
 if __name__ == "__main__":
