15d47366313aa64f747a750f7c389feddd3f37be
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2024-10-16 17:21:09 +0800

    Update claude.py

--- rev_claude/client/claude.py
+++ rev_claude/client/claude.py
@@ -298,7 +298,8 @@ class Client:
             [f"{message['role']}: {message['content']}" for message in messages]
         )
         response_text = ""
-
+        if get_poe_bot_info()[model.lower().get("text2image", False)]:
+            messages_str = prompt
 
         poe_bot_client = await AsyncPoeApi(tokens=self.tokens).create()
         try:
