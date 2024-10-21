cb8a85b6999ab028805857b7debfe94aab7a5df5
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2024-10-21 16:52:09 +0800

    Update poe_router.py

--- rev_claude/poe_router/poe_router.py
+++ rev_claude/poe_router/poe_router.py
@@ -15,10 +15,13 @@ router = APIRouter()
 
 
 @router.post("/get_available_bots")
-async def get_available_bots():
+async def get_available_bots(
+        count: int = 25,
+        get_all: bool = False,
+):
     basic_clients, plus_clients = ClientManager().get_clients()
     client = next(iter(plus_clients.values()))
     poe_client: AsyncPoeApi = client.poe_bot_client
-    all_bots = await poe_client.get_available_bots(count=25, get_all=False)
+    all_bots = await poe_client.get_available_bots(count=count, get_all=get_all)
     return all_bots
     # basic_clients = [client.dict() for client in basic_clients.values()]
