b074b9999b41a5ef38d16a74d995db5c8e6d6602
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2024-10-22 10:47:16 +0800

    feature: add the bot list and the bot information retrival tool

--- rev_claude/poe_router/poe_router.py
+++ rev_claude/poe_router/poe_router.py
@@ -25,3 +25,17 @@ async def get_available_bots(
     all_bots = await poe_client.get_available_bots(count=count, get_all=get_all)
     return all_bots
     # basic_clients = [client.dict() for client in basic_clients.values()]
+
+@router.post("/get_botInfo")
+async def get_botInfo(
+        handle: str
+):
+    basic_clients, plus_clients = ClientManager().get_clients()
+    client = next(iter(plus_clients.values()))
+    poe_client: AsyncPoeApi = client.poe_bot_client
+    bot_infor = await poe_client.get_botInfo(handle=handle)
+    return bot_infor
+    # basic_clients = [client.dict() for client in basic_clients.values()]
+
+
+
