215a65a95666edb27ed4c3f6a6202011385fdaa9
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2024-10-22 10:00:21 +0800

    Update utils.py

--- rev_claude/update_bots_infor/utils.py
+++ rev_claude/update_bots_infor/utils.py
@@ -2,19 +2,19 @@ from rev_claude.client.client_manager import ClientManager
 from rev_claude.poe_api_wrapper import AsyncPoeApi
 from loguru import logger
 
-def get_first_plus_client() -> AsyncPoeApi:
+async def get_first_plus_client() -> AsyncPoeApi:
     basic_clients, plus_clients = ClientManager().get_clients()
     logger.debug(f"basic_clients: \n{basic_clients}")
     logger.debug(f"plus_clients: \n{plus_clients}")
     client = list(plus_clients.values())[0]
-    return client.poe_bot_client
+    return await client.get_poe_bot_client()
 
 
 async def get_available_bots(
     count: int = 25,
     get_all: bool = False,
 ):
-    poe_client: AsyncPoeApi = get_first_plus_client()
+    poe_client: AsyncPoeApi = await get_first_plus_client()
     all_bots = await poe_client.get_available_bots(count=count, get_all=get_all)
     return all_bots
 
@@ -23,6 +23,6 @@ async def get_available_bots(
 
 
 async def get_bot_information(handle: str):
-    poe_client: AsyncPoeApi = get_first_plus_client()
+    poe_client: AsyncPoeApi = await get_first_plus_client()
     bot_info = await poe_client.get_botInfo(handle=handle)
     return bot_info
