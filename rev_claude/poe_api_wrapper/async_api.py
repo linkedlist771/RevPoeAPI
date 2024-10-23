6e0f38bb3fca0bb96bebe64c17c20e14df6a48f1
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2024-10-23 16:47:01 +0800

    Feature: add async message queue

--- rev_claude/poe_api_wrapper/async_api.py
+++ rev_claude/poe_api_wrapper/async_api.py
@@ -50,7 +50,7 @@ class AsyncPoeApi:
         self.ws_connected: bool = False
         self.ws_error: bool = False
         self.active_messages: dict[int, str] = {}
-        self.message_queues: dict[int, queue.Queue] = {}
+        self.message_queues = {}
         self.current_thread: dict[str, list] = {}
         self.retry_attempts: int = 3
         self.ws_refresh: int = 3
@@ -405,12 +405,26 @@ class AsyncPoeApi:
                     return
 
                 if chat_id in self.message_queues:
-                    self.message_queues[chat_id].put(
-                        {
+                    # self.message_queues[chat_id].put(
+                    #     {
+                    #         "data": data,
+                    #         "subscription": subscriptionName,
+                    #     }
+                    # )
+                    # await self.message_queues[chat_id].put(
+                    #     {
+                    #             "data": data,
+                    #             "subscription": subscriptionName,
+                    #         }
+                    # )
+                    asyncio.run_coroutine_threadsafe(
+                        self.message_queues[chat_id].put({
                             "data": data,
                             "subscription": subscriptionName,
-                        }
+                        }),
+                        self.loop  # 需要在类中保存事件循环的引用
                     )
+
                     if subscriptionName == "messageAdded":
                         self.active_messages[chat_id] = data["messageAdded"][
                             "messageId"
@@ -428,7 +442,16 @@ class AsyncPoeApi:
         self.loop.run_in_executor(None, self.connect_ws())
 
     async def delete_queues(self, chatId: int):
+        # if chatId in self.message_queues:
+        #     del self.message_queues[chatId]
+        # if chatId in self.active_messages:
+        #     del self.active_messages[chatId]
         if chatId in self.message_queues:
+            while not self.message_queues[chatId].empty():
+                try:
+                    await self.message_queues[chatId].get_nowait()
+                except asyncio.QueueEmpty:
+                    pass
             del self.message_queues[chatId]
         if chatId in self.active_messages:
             del self.active_messages[chatId]
@@ -805,7 +828,8 @@ class AsyncPoeApi:
             )
 
         self.active_messages[chatId] = None
-        self.message_queues[chatId] = queue.Queue()
+        # self.message_queues[chatId] = queue.Queue()
+        self.message_queues[chatId] = asyncio.Queue()
 
         last_text = ""
         stateChange = False
@@ -814,18 +838,33 @@ class AsyncPoeApi:
         suggestedReplies = []
 
         while True:
+            # try:
+            #     ws_data = self.message_queues[chatId].get(timeout=timeout)
+            # except KeyError:
+            #     await asyncio.sleep(1)
+            #     continue
+            # except queue.Empty:
+            #     try:
+            #         if self.retry_attempts > 0:
+            #             self.retry_attempts -= 1
+            #             logger.warning(
+            #                 f"Retrying request {3-self.retry_attempts}/3 times..."
+            #             )
+            #         else:
+            #             self.retry_attempts = 3
+            #             await self.delete_queues(chatId)
+            #             raise RuntimeError("Timed out waiting for response.")
+            #         await self.connect_ws()
+            #         continue
+            #     except Exception as e:
+            #         raise e
             try:
-                ws_data = self.message_queues[chatId].get(timeout=timeout)
-            except KeyError:
-                await asyncio.sleep(1)
-                continue
-            except queue.Empty:
+                ws_data = await asyncio.wait_for(self.message_queues[chatId].get(), timeout=timeout)
+            except asyncio.TimeoutError:
                 try:
                     if self.retry_attempts > 0:
                         self.retry_attempts -= 1
-                        logger.warning(
-                            f"Retrying request {3-self.retry_attempts}/3 times..."
-                        )
+                        logger.warning(f"Retrying request {3 - self.retry_attempts}/3 times...")
                     else:
                         self.retry_attempts = 3
                         await self.delete_queues(chatId)
@@ -1113,7 +1152,8 @@ class AsyncPoeApi:
                 raise e
 
         self.active_messages[chatId] = None
-        self.message_queues[chatId] = queue.Queue()
+        # self.message_queues[chatId] = queue.Queue()
+        self.message_queues[chatId] = asyncio.Queue()
 
         last_text = ""
         stateChange = False
@@ -1122,18 +1162,33 @@ class AsyncPoeApi:
         suggestedReplies = []
 
         while True:
+            # try:
+            #     ws_data = self.message_queues[chatId].get(timeout=timeout)
+            # except KeyError:
+            #     await asyncio.sleep(1)
+            #     continue
+            # except queue.Empty:
+            #     try:
+            #         if self.retry_attempts > 0:
+            #             self.retry_attempts -= 1
+            #             logger.warning(
+            #                 f"Retrying request {3-self.retry_attempts}/3 times..."
+            #             )
+            #         else:
+            #             self.retry_attempts = 3
+            #             await self.delete_queues(chatId)
+            #             raise RuntimeError("Timed out waiting for response.")
+            #         await self.connect_ws()
+            #         continue
+            #     except Exception as e:
+            #         raise e
             try:
-                ws_data = self.message_queues[chatId].get(timeout=timeout)
-            except KeyError:
-                await asyncio.sleep(1)
-                continue
-            except queue.Empty:
+                ws_data = await asyncio.wait_for(self.message_queues[chatId].get(), timeout=timeout)
+            except asyncio.TimeoutError:
                 try:
                     if self.retry_attempts > 0:
                         self.retry_attempts -= 1
-                        logger.warning(
-                            f"Retrying request {3-self.retry_attempts}/3 times..."
-                        )
+                        logger.warning(f"Retrying request {3 - self.retry_attempts}/3 times...")
                     else:
                         self.retry_attempts = 3
                         await self.delete_queues(chatId)
