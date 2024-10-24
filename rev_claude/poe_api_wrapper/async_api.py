26c44bf0f85fa29ba38f487fe107259866b3d34e
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2024-10-24 20:46:04 +0800

    Update async_api.py

--- rev_claude/poe_api_wrapper/async_api.py
+++ rev_claude/poe_api_wrapper/async_api.py
@@ -1057,6 +1057,7 @@ class AsyncPoeApi:
                             yield chunk
                         return
 
+                    logger.debug(f"message_data:\n{message_data}")
                     logger.info(
                         f"New Thread created | {message_data['data']['messageEdgeCreate']['chat']['chatCode']}"
                     )
