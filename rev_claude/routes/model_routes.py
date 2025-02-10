2a19f109f5f7ad5539df64cfcd5ed308a892f1fe
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2025-02-10 22:34:23 +0800

    Update model_routes.py

--- rev_claude/routes/model_routes.py
+++ rev_claude/routes/model_routes.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from rev_claude.configs import ROOT
 from rev_claude.utils.poe_bots_utils import get_poe_bot_info
 
+
 router = APIRouter()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory=ROOT / "templates")
 
 @router.get("/models")
 async def show_models(request: Request):
